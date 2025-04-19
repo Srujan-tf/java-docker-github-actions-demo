c4b7a13c8bf5b63859f94d0cf55a1d3b18720d28
srujan-tf <srujanreddy4810@gmail.com>
2025-04-19 13:21:44 +1000

    Added jave project files

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions + Docker!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
